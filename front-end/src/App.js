5c7fbf69aa16a2df010a2f3fa191002a6007dc91
Jed Piezas <jed.piezas@gmail.com>
2020-04-10 15:50:49 -0700

    Remove .css files and move everything to styled-components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,51 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-import Signup from "./components/Signup/Signup";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   );
-}
-
-function Home() {
-  return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  );
-}
-
-function Foobar() {
-  return <div>This is the Foobar page</div> ;
 }
 
 export default App;
