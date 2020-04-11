df46e1d976d85aff8970c29cfff98efe684af5ab
Jed Piezas <jed.piezas@gmail.com>
2020-04-10 22:09:51 -0700

    Add signup page

--- front-end/src/components/Signup/index.js
+++ front-end/src/components/Signup/index.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import FormInput from '../../shared/FormInput';
+import Logo from '../../svg/Logo';
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      phoneNumber: '',
+    };
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value);
+  }
+  
   render() {
     return (
       <SignupContainer>
         <LeftPanel>
+          <Logo fill="#FFFFFF" height="25%" />
           <div>
             Welcome to the Github Connector App
           </div>
         </LeftPanel>
-        <Form>
-          <FormHeading>
-            Create your account
-          </FormHeading>
-        </Form>
+        <RightPanel>
+          <FormContainer>
+            <FormHeading>
+              Create your account
+            </FormHeading>
+            <FormInput heading="Username" type="text" value={this.state.username} onChange={this.handleChange} />
+            <SubmitButton type="submit" value="Create account"/>
+          </FormContainer>
+        </RightPanel>
       </SignupContainer>
     )
   }
@@ -28,24 +49,43 @@ const LeftPanel = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   width: 40vw;
   height: 100vh;
-  background: #1a2e38;
+  background: #1A2E38;
   color: #fff;
   font-size: 16px;
 `;
 
-const Form = styled.div`
+const RightPanel = styled.div`
   display: flex;
   flex-direction: column;
-  color: #000;
+  justify-content: center;
+  align-items: center;
   width: 60vw;
+`;
+
+const FormContainer = styled.div`
+  width: 30vw;
 `;
 
 const FormHeading = styled.div`
   font-size: 24px;
   font-weight: 400;
-  color: #2c3a41;
+  color: #2C3A41;
+  margin-bottom: 32px;
+`;
+
+const SubmitButton = styled.input`
+  border: none;
+  color: #FFFFFF;
+  background: #7F337E;
+  height 40px;
+  width: 100%;
+  font-size: 16px;
+  font-weight: bold;
+  border-radius: 4px;
+  margin-top: 32px;
 `;
 
 export default Signup;
