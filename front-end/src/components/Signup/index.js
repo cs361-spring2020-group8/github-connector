b93b60548dc4f2301eec223ea03174bf4e07f955
Jed Piezas <jed.piezas@gmail.com>
2020-04-10 20:42:02 -0700

    Add left-panel and heading

--- front-end/src/components/Signup/index.js
+++ front-end/src/components/Signup/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-function Signup() {
+class Signup extends React.Component {
+  render() {
     return (
-        <div>Hello this is the signup page!</div>
+      <SignupContainer>
+        <LeftPanel>
+          <div>
+            Welcome to the Github Connector App
+          </div>
+        </LeftPanel>
+        <Form>
+          <FormHeading>
+            Create your account
+          </FormHeading>
+        </Form>
+      </SignupContainer>
     )
+  }
 }
+
+const SignupContainer = styled.div`
+  display: flex;
+`;
+
+const LeftPanel = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40vw;
+  height: 100vh;
+  background: #1a2e38;
+  color: #fff;
+  font-size: 16px;
+`;
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #000;
+  width: 60vw;
+`;
+
+const FormHeading = styled.div`
+  font-size: 24px;
+  font-weight: 400;
+  color: #2c3a41;
+`;
 
 export default Signup;
