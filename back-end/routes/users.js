88c3fa0854be83b26949d6b8fba3a4e2e1db40c1
Michele Costello <13283117+mcmoonstruck@users.noreply.github.com>
2020-04-12 16:57:48 -0700

    Add backend server on heroku

---
+++ back-end/routes/users.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+const { pool } = require('../config')
+
+const makeDbQueryAndReturnResults = (queryString, res) => {
+  pool.query(queryString, (error, results) => {
+    if (!results || !results.rows || results.rows[0] === undefined || error) {
+      return res.status(500).send('An error occurred');
+    }
+    return res.status(200).send(results.rows);
+  })
+}
+
+const makeDbQueryAndReturnStatus = (queryString, res) => {
+  pool.query(queryString, (error, results) => {
+    if (error) {
+      return res.status(500).send('An error occurred');
+    }
+    return res.status(200);
+  })
+}
+
+/* GET all users */
+router.get('/', function(req, res, next) {
+  const getAllUsersQuery = 'SELECT * FROM users';
+  makeDbQueryAndReturnResults(getAllUsersQuery, res);
+});
+
+/* GET user by ID */
+router.get('/:id', function(req, res, next) {
+  const getUserQuery = 'SELECT * FROM users WHERE id=' + req.params.id;
+  makeDbQueryAndReturnResults(getUserQuery, res);
+});
+
+/* POST to create new users */
+router.post('/', function(req, res, next) {
+  const {
+    username, password, github_username
+  } = req.body;
+
+  let createUserQuery = "INSERT INTO users(username, password, github_username, created_at) VALUES("
+  createUserQuery += "'" + username + "', '" + password + "', '"+ github_username + "', CURRENT_TIMESTAMP"
+  createUserQuery += ") returning *"
+  makeDbQueryAndReturnResults(createUserQuery, res);
+});
+
+/* DELETE user by ID. */
+router.delete('/:id', function(req, res, next) {
+  const deleteUserQuery = 'DELETE FROM users WHERE id=' + req.params.id;
+  makeDbQueryAndReturnStatus(deleteUserQuery, res);
+});
+
+
+module.exports = router;
