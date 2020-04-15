91a6b191902b7e93c8dcc2c95ee90ed04eaa9da7
Andre <andrepaul1993@gmail.com>
2020-04-15 11:25:18 -0700

    helmet, compression, cors, rate limits

--- back-end/app.js
+++ back-end/app.js
@@ -7,7 +7,30 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const{body, check} = require('express-validator');
+
 const app = express();
+
+app.use(compression());
+app.use(helmet());
+
+// uses CORS on URL if isProduction is true.
+const isProduction = process.env.NODE_ENV === 'production';
+const origin = {
+    origin: isProduction ? 'https://githubconnector.herokuapp.com' : '*',
+};
+app.use(cors(origin));
+
+// limits a client to 10 requests per minute.
+const limiter = rateLimit({
+    windowMs: 1*60*1000,
+    max: 10,
+});
+app.use(limiter);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
