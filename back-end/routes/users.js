56adbaf51d30c5854ece0356028cab6d8c5ead77
Andre <andrepaul1993@gmail.com>
2020-04-15 11:50:28 -0700

    jwt mockup

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 const { pool } = require('../config');
 const validator = require('express-validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const expjwt = require('express-jwt');
 
 const makeDbQueryAndReturnResults = (queryString, res) => {
   pool.query(queryString, (error, results) => {
@@ -33,6 +35,56 @@ router.get('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
   const getUserQuery = 'SELECT * FROM users WHERE id=' + req.params.id;
   makeDbQueryAndReturnResults(getUserQuery, res);
+});
+
+/* POST for logging in */
+// TODO: fix this with an .env variable?
+// jwt middleware, just fake for now until I figure out proper way:
+// read that it normally should be a .env variable.
+const jwtMW = expjwt({
+  secret: 'a fake key for demo purposes'
+});
+
+router.post('/login', function (req, res, next) {
+  const {
+    username, password
+  } = req.body;
+  // TODO: whats the best way to query DB for existing user?
+  // Just pseudo code for now...
+  // TODO: remember to hash / unhash password.
+  // fake DB entry just for demo purposes:
+  const user = {
+    id: 1,
+    username: fakeUser,
+    password: fakePassword,
+  };
+
+  if(username == user.username && password == user.password){
+    let token = jwt.sign({
+      id: user.id,
+      username: user.username
+    }, 'a fake key for demo purposes',
+        {
+          expiresIn: 123456
+        });
+    res.json({
+      success: true,
+      err: null,
+      token
+    });
+  }else{
+    res.status(401).json({
+      success: false,
+      token: null,
+      err: 'Username or password incorrect.'
+    });
+  }
+});
+
+/* mockup using the literal jwtMW above */
+// TODO: modify this to be a real route w/ real middleware.
+router.get('/', jwtMW, function (req, res, next) {
+  res.send('Authentication successful.');
 });
 
 /* POST to create new users */
