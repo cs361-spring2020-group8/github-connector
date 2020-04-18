54216229f35c6a17ba6c8d571b7f51c55ceddf0c
Paul Newling - iMac <PaulNewling@Gmail.com>
2020-04-18 13:35:07 -0700

    A very basic dashboard pages that has a basic sidenav that is only div's at the moment and a content box that is empty.

--- front-end/src/components/Dashboard/index.js
+++ front-end/src/components/Dashboard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logo from '../../svg/Logo';
-import FormInput from "../../shared/FormInput";
+//import FormInput from "../../shared/FormInput";
 
 class Dashboard extends React.Component {
     constructor(props) {
@@ -26,19 +26,25 @@ class Dashboard extends React.Component {
                         <div>TEST-FRIENDS</div>
                         <div>TEST-???</div>
                         <div>TEST-LINK</div>
+                        <div>TEST-SIGNOUT</div>
+                        <div>I envision these <br/>
+                            having space between<br/>
+                            them when they<br/>
+                            become links</div>
                     </NavPanel>
+                    <FooterPanel></FooterPanel>
                 </LeftPanel>
                 <RightPanel>
                     <DashboardContentContainer>
-                        <FormHeading>
+                        <DashboardContentHeading>
                             Create your account
-                        </FormHeading>
-                        <FormContent>
+                        </DashboardContentHeading>
+                        <DashboardContent>
+                            <div>TEST - This color is only to show where there content box is...</div>
                             <div>TEST</div>
                             <div>TEST</div>
                             <div>TEST</div>
-                            <div>TEST</div>
-                        </FormContent>
+                        </DashboardContent>
                     </DashboardContentContainer>
                 </RightPanel>
             </DashboardContainer>
@@ -48,6 +54,7 @@ class Dashboard extends React.Component {
 
 const DashboardContainer = styled.div`
   display: flex;
+  min-height: 100vh;
 `;
 
 const LeftPanel = styled.div`
@@ -73,12 +80,18 @@ const LogoBox = styled.div`
 const NavPanel = styled.div`
     display: flex;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-between;
     flex-direction: column;
     background: #1A2E38;
     color: #fff;
     font-size: 16px;
+    margin: 10px;
     
+`;
+
+
+const FooterPanel = styled.div`
+    display: flex;
 `;
 
 const RightPanel = styled.div`
@@ -87,25 +100,25 @@ const RightPanel = styled.div`
   justify-content: center;
   align-items: center;
   width: 80vw;
-  height: 80vw;
+  height: 100vh;
   
 `;
 
 const DashboardContentContainer = styled.div`
   width: 70vw;
-  height: 60vw;
+  height: 60vh;
 
   background: #BBBBBB;
 `;
 
-const FormHeading = styled.div`
+const DashboardContentHeading = styled.div`
   font-size: 24px;
   font-weight: 400;
   color: #2C3A41;
   margin-bottom: 32px;
 `;
 
-const FormContent = styled.div`
+const DashboardContent = styled.div`
    font-size: 24px;
    font-weight: 400;
    color: #2C3A41;
