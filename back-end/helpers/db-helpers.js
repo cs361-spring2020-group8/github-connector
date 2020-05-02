5d4262f86b7fe891effc02df01dd9750b1bb2b99
Michele Costello <13283117+mcmoonstruck@users.noreply.github.com>
2020-05-02 16:16:08 -0700

    Additional cleanup to remove unused imports and exports.

--- back-end/helpers/db-helpers.js
+++ back-end/helpers/db-helpers.js
@@ -1,6 +1,5 @@
 // helper functions to query db and return standardized API responses
 const { pool } = require('../config');
-const { returnErrorWithMessage, returnGeneralError} = require('./response-helpers');
 
 async function makeDbQuery(queryString) {
   try {
@@ -20,38 +19,8 @@ async function getRowFromDb(queryString) {
   return results.rows[0];
 };
 
-async function getUserByEmail(email) {
-  const query = `SELECT password, id FROM users WHERE email = '${email}'`;
-  const userData = await getRowFromDb(query);
-  return userData;
-}
-
-async function getUserGitHubInfo(userID) {
-  const userGitHubInfoQuery = `SELECT * FROM github_info WHERE user_id=${userID}`;
-  const userGitHubData = await getRowFromDb(userGitHubInfoQuery);
-  return userGitHubData;
-}
-
-async function getFullUserProfile(userID) {
-  const userQuery = `SELECT * FROM users WHERE id=${userID}`;
-  const userData = await getRowFromDb(userQuery);
-
-  // exit early if user isn't in the database
-  if(!userData) {
-    return null;
-  }
-
-  //omit password from API responses
-  delete userData.password;
-
-  const userGitHubData = await getUserGitHubInfo(userID);
-  userData.github_info = userGitHubData ? userGitHubData : null;
-  return userData;
-};
 
 module.exports = {
    makeDbQuery,
    getRowFromDb,
-   getUserByEmail,
-   getFullUserProfile,
 }
