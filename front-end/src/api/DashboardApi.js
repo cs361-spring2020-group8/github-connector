b515ab996df072164637ed6210ceb42570fd9e3a
Paul Newling - iMac <PaulNewling@Gmail.com>
2020-05-05 09:09:28 -0700

    Removed commented out code

--- front-end/src/api/DashboardApi.js
+++ front-end/src/api/DashboardApi.js
@@ -25,24 +25,6 @@ export async function linkGithubAccount(userID, token, github_username) {
 }
 
 
-/* Couldn't get all of them to be updated at once so I parsed them out individually
-
-export async function updateProfileInformation(userID, token, email, twitter, phone) {
-  const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://githubconnector.herokuapp.com';
-  const config = {
-    headers:{
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '  + token }
-  };
-  const payload = {
-    'email':email,
-    'twitter': twitter,
-    'phone': phone
-  };
-  return axios.put(`${url}/users/${userID}`, payload, config);
-}*/
-
 // Updates Profile Information
 export async function updateEmailInformation(userID, token, email) {
   const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://githubconnector.herokuapp.com';
