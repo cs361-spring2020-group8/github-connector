78d82ea40e122f1d2c2dd82492302ab05f350903
Jonathan Abantao <jonabantao@gmail.com>
2020-05-06 17:19:43 -0700

    Dockerize project

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -4,6 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (process.env.NODE_ENV === 'dockerlocal') {
+    return res.redirect(`http://localhost:${process.env.REACT_PORT}`);
+  }
+
   res.sendFile(path.join(__dirname, '../../dist/index.html'))
 });
 
